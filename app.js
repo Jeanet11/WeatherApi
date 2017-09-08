9252761981269b39038b5d24f3a13ce6b677e705
Jeanne <jeanne.tiramani@hotmail.fr>
2017-09-08 15:54:06 +0200

    suppression de la clé APPI

--- app.js
+++ app.js
@@ -8,18 +8,3 @@ $.ajax({
 		console.log(data);
 	}
 })
-// console.log(data);    
-// $('#meteo').html(data);    });
-// $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Carcassonne&cnt=3&mode=html&appid=478ace2ac65f244edad1064f666e3bd0').then( function (data) {
-//     console.log(data);    
-// });
-// $('#meteo').html(data); 
-
-
-// $.ajax({
-//     url: "http://api.openweathermap.org/data/2.5/weather?q=Carcassonne&cnt=3&mode=html&appid=478ace2ac65f244edad1064f666e3bd0",
-    
-// }).done(function (data) {
-//     console.log(data);
-
-// });
